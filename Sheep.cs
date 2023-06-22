31885f8ac7a188b571439646bc5438c7eec04bd9
Alejandra Roxana Rosas Nieves <20203tn108@utez.edu.mx>
2023-06-21 20:02:44 -0600

    fix: update class Sheep - conflict resolved

---
+++ Sheep.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Sheep : IAnimal
+{
+    public void MakeSound()
+    {
+        Console.WriteLine("English: Baa! Baa! Baa!");
+        Console.WriteLine("Spanish: Beee! Beee! Beee!");
+    }
+}
